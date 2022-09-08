72554b423c1600bfd04bfd02bcf7ce6cbe546db3
guu13 <barry.wang0520@gmail.com>
2022-09-08 19:29:26 +0800

    mylog fix 2

--- mylog.go
+++ mylog.go
@@ -3,8 +3,9 @@ package mylog
 import "os"
 
 type Mylogger struct {
-	Name string
-	Path string
+	AppName string
+	Path    string
+	Logfile string
 }
 
 var (
@@ -12,17 +13,23 @@ var (
 )
 
 func New() *Mylogger {
-	return &Mylogger{Name: "mylog", Path: "/var/log/"}
+	return &Mylogger{AppName: "mylog", Path: "/var/log/", Logfile: "/var/log/mylog.log"}
 }
 
-func SetName(Name string) {
-	mylogger.Name = Name
+func SetName(AppName string, LogName string) {
+	mylogger.AppName = AppName
+	mylogger.Logfile = mylogger.Path + LogName + ".log"
+}
+
+func SetAppName(AppName string) {
+	mylogger.AppName = AppName
+	mylogger.Logfile = mylogger.Path + AppName + ".log"
 }
 
 func Log2file(log string) {
 
 	//创建日志文件
-	f, err := os.OpenFile(mylogger.Path+mylogger.Name+".log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	f, err := os.OpenFile(mylogger.Logfile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		return
 	}
@@ -31,7 +38,7 @@ func Log2file(log string) {
 	defer f.Close()
 
 	// 字符串写入
-	_, err = f.WriteString(mylogger.Name + " " + log + "\n")
+	_, err = f.WriteString(mylogger.AppName + ":::" + log + "\n")
 	if err != nil {
 		return
 	}
